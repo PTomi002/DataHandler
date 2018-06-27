b99917765999a1d4f3713a4c8dbba8fedb7eb7ff
Tamas David Paulin <tamas.paulin@evosoft.com>
2018-06-27 13:00:57 +0200

    init

---
+++ src/main/java/hu/exercise/microservice/DataHandler/DataHandlerApplication.java
@@ -0,0 +1,12 @@
+package hu.exercise.microservice.DataHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataHandlerApplication.class, args);
+	}
+}
